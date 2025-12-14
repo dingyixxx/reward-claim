e73cdb1e9da625bae44bd831fbb6efcb4822b4ea
dingyi <dingyixxx@126.com>
2025-12-14 15:12:07 +0800

    init

--- award-service/src/main/java/com/example/AwardServiceApplication.java
+++ award-service/src/main/java/com/example/AwardServiceApplication.java
@@ -2,14 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.example.client")
 public class AwardServiceApplication {
+    @Bean
+    public HttpMessageConverters httpMessageConverters() {
+        return new HttpMessageConverters();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AwardServiceApplication.class, args);
