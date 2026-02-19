18dd001f10e4bc2f6933ff72fa24ed240ac35101
dingyi <dingyixxx@126.com>
2026-02-19 20:30:05 +0800

    add full skywalking agent path

--- module1-controller/src/main/java/module1controller/Module1Application.java
+++ module1-controller/src/main/java/module1controller/Module1Application.java
@@ -10,4 +10,7 @@ public class Module1Application {
     public static void main(String[] args) {
         SpringApplication.run(Module1Application.class, args);
     }
+//    -javaagent:C:\Users\P72\Downloads\skywalking-agent\skywalking-agent.jar
+//-Dskywalking.agent.service_name=consumer-app
+//-Dskywalking.collector.backend_service=192.168.200.131:11800
 }
